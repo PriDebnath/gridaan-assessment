5f2eca2996f27acde3722eac2f8db9cf8538345f
Pritam <debnathpritam0802@gmail.com>
2026-04-09 17:04:44 +0530

    created api sign up

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express from "express";
+import authRoutes from "./routes/auth.routes";
 import studentRoutes from "./routes/student.routes";
 
 const app = express();
@@ -11,6 +12,7 @@ app.get("/", (_req, res) => {
   res.json({ message: "success" });
 });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/students", studentRoutes);
 
 export default app;
---
+++ backend/src/controllers/auth.controllers.ts
@@ -0,0 +1,52 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import { adminModel as model, Admin as Type } from "../models";
+
+export const signUp = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) return res.status(400).json({ message: "Missing data" });
+
+    const user = await model.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already present" })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    if (!hashedPassword) return res.status(400).json({ message: "Could not hash password" });
+
+    const newData = new model({
+      email,
+      hashedPassword,
+      created_at: Date.now(),
+      modified_at: Date.now(),
+    });
+
+    const saved = await newData.save();
+    res.status(201).json({ saved });
+  } catch (error) {
+    res.status(500).json({ message: "Error in sign up" });
+  }
+};
+
+export const signIn = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await model.findOne({ email });
+
+    if (!user || !user?.password) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+    const JWT_SECRET: string = "pritam"
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1d" });
+
+    res.json({ token });
+  } catch (error: any) {
+    res.status(500).json({ message: error?.message });
+  }
+};
