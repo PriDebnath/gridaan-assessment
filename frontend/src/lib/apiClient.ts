290b8c163dd4b43fd9738f703775b62d84d011b3
Pritam <debnathpritam0802@gmail.com>
2026-04-09 19:12:52 +0530

    fetch wrapper to attach jwt

---
+++ frontend/src/lib/apiClient.ts
@@ -0,0 +1,32 @@
+import { BASE_API_URL } from "@/enviroments";
+import { useAuthStore } from "@/store/auth.store";
+
+export async function apiClient<T>(
+  endpoint: string,
+  options: RequestInit = {}
+): Promise<T> {
+  const { token, setToken} = useAuthStore()
+  // const token = localStorattge.getItem("token");
+
+  const res = await fetch(`${BASE_API_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(token && { Authorization: `Bearer ${token}` }),
+      ...options.headers,
+    },
+  });
+
+  // 🔐 Handle auth errors globally
+  if (res.status === 401) {
+    setToken("");
+    window.location.href = "/";
+  }
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message || "API Error");
+  }
+
+  return res.json();
+}
--- frontend/src/provider/query-client.provider.tsx
+++ frontend/src/provider/query-client.provider.tsx
@@ -2,7 +2,18 @@
 //@Link: https://tanstack.com/query/latest/docs/framework/react/quick-start
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5, // 5 min cache
+      refetchOnWindowFocus: false,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+})
 
 export function TanstackQueryClientProvider(props: { children: React.ReactNode }) {
   return (
